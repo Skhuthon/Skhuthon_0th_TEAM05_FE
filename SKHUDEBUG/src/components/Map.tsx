85471c35a035b40e0a46cee002868f4accd77886
leemanjae02 <144561017+leemanjae02@users.noreply.github.com>
2024-06-29 19:22:21 +0900

    Feat: 구글 맵 api 반경 기능 구현

--- SKHUDEBUG/src/components/Map.tsx
+++ SKHUDEBUG/src/components/Map.tsx
@@ -1,25 +1,168 @@
-import { GoogleMap, LoadScript } from "@react-google-maps/api";
+import { useState } from "react";
+import { GoogleMap, LoadScript, Autocomplete } from "@react-google-maps/api";
+import styles from "../styles/Map.module.less";
+import { useEffect } from "react";
 
-const mapContainerStyle = {
-  width: "100%",
-  height: "100%",
-};
+interface MapProps {
+  setLatitude: (latitude: number) => void;
+  setLongitude: (longitude: number) => void;
+  onChangeRadius: (radius: React.ChangeEvent<HTMLInputElement>) => void;
+  radius: number;
+  latitude: number;
+  longitude: number;
+}
 
 const center = {
-  lat: -3.745,
-  lng: -38.523,
+  lat: 37.566535,
+  lng: 126.977992,
 };
 
-const MapComponent = () => {
+const myStyles = [
+  {
+    featureType: "poi",
+    elementType: "labels",
+    stylers: [{ visibility: "off" }],
+  },
+];
+
+const MapWithSearch: React.FunctionComponent<MapProps> = ({
+  setLatitude,
+  setLongitude,
+  onChangeRadius,
+  radius,
+  latitude,
+  longitude,
+}) => {
+  const [autocomplete, setAutocomplete] =
+    useState<google.maps.places.Autocomplete | null>(null);
+  const [map, setMap] = useState<google.maps.Map | null>(null);
+  const [circle, setCircle] = useState<google.maps.Circle | null>(null);
+  const [marker, setMarker] = useState<google.maps.Marker | null>(null);
+  const onLoad = (autocompleteInstance: google.maps.places.Autocomplete) => {
+    setAutocomplete(autocompleteInstance);
+  };
+
+  useEffect(() => {
+    if (circle) {
+      circle.setRadius(radius);
+    }
+  }, [radius, circle]);
+
+  console.log(latitude, longitude);
+
+  const onPlaceChanged = () => {
+    if (autocomplete !== null) {
+      const place = autocomplete.getPlace();
+      if (place.geometry && place.geometry.location) {
+        const location = place.geometry.location;
+        map?.panTo(location);
+        map?.setZoom(14);
+        setLatitude(location.lat());
+        setLongitude(location.lng());
+
+        if (circle) {
+          circle.setCenter(location);
+        } else {
+          const newCircle = new google.maps.Circle({
+            center: location,
+            radius: radius,
+            strokeColor: "red",
+            strokeOpacity: 0.6,
+            fillColor: "gray",
+            fillOpacity: 0.5,
+            map: map,
+          });
+
+          setCircle(newCircle);
+        }
+
+        if (marker) {
+          marker.setPosition(location);
+        } else {
+          console.log(map);
+        }
+        const newMarker = new google.maps.Marker({
+          position: { lat: latitude, lng: longitude },
+          map: map,
+          icon: {
+            url: "logo.png",
+            scaledSize: new window.google.maps.Size(32, 32),
+          },
+        });
+        setMarker(newMarker);
+      }
+    } else {
+      console.log("로딩 중");
+    }
+  };
+
   return (
-    <LoadScript googleMapsApiKey={import.meta.env.VITE_MAP_API}>
-      <GoogleMap
-        mapContainerStyle={mapContainerStyle}
-        center={center}
-        zoom={10}
-      ></GoogleMap>
+    <LoadScript
+      googleMapsApiKey={import.meta.env.VITE_MAP_API}
+      libraries={["places"]}
+    >
+      <div>
+        <Autocomplete onLoad={onLoad} onPlaceChanged={onPlaceChanged}>
+          <div className={styles.AutoComplateInput}>
+            <input type="text" placeholder="장소를 검색하세요" />
+            <div className={styles.radiusBtn}>
+              <label htmlFor="radius1">
+                <p>100m</p>
+                <input
+                  type="radio"
+                  id="radius1"
+                  name="radius"
+                  value="100"
+                  onChange={onChangeRadius}
+                />
+              </label>
+              <label htmlFor="radius2">
+                <p>300m</p>
+                <input
+                  type="radio"
+                  id="radius2"
+                  name="radius"
+                  value="300"
+                  onChange={onChangeRadius}
+                />
+              </label>
+              <label htmlFor="radius3">
+                <p>500m</p>
+                <input
+                  type="radio"
+                  id="radius3"
+                  name="radius"
+                  value="500"
+                  onChange={onChangeRadius}
+                />
+              </label>
+            </div>
+          </div>
+        </Autocomplete>
+        <div className={styles.MapStyle}>
+          <GoogleMap
+            options={{ disableDefaultUI: true, styles: myStyles }}
+            mapContainerStyle={{ width: "100%", height: "100%" }}
+            center={center}
+            zoom={10}
+            onLoad={(mapInstance) => {
+              setMap(mapInstance);
+              const initialCircle = new google.maps.Circle({
+                center: center,
+                radius: 500,
+                strokeColor: "blue",
+                strokeOpacity: 0.6,
+                fillColor: "white",
+                fillOpacity: 0.5,
+                map: mapInstance,
+              });
+              setCircle(initialCircle);
+            }}
+          />
+        </div>
+      </div>
     </LoadScript>
   );
 };
 
-export default MapComponent;
+export default MapWithSearch;
--- SKHUDEBUG/src/service/UserService.ts
+++ SKHUDEBUG/src/service/UserService.ts
@@ -39,3 +39,43 @@ export const login = async (id: string, password: string) => {
     throw error;
   }
 };
+
+export const findHunter = async (
+  loginId: string,
+  latitude: number,
+  longitude: number,
+  bugNum: number,
+  bugSize: string,
+  bugType: string,
+  radius: number
+) => {
+  try {
+    const response = await CustomAxios.post(
+      "/bughunt",
+      {
+        loginId: loginId,
+        latitude: latitude,
+        longitude: longitude,
+        bugNum: bugNum,
+        bugSize: bugSize,
+        bugType: bugType,
+        radius: radius,
+      },
+      {
+        headers: {
+          "Content-Type": "application/json",
+        },
+      }
+    );
+
+    return response;
+  } catch (error) {
+    console.log(error);
+    throw error;
+  }
+};
+
+export const getMaker = async (loginId: string) => {
+  const response = await CustomAxios.get(`/bughunt/user?loginId=${loginId}`);
+  return response;
+};
